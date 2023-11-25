aac61fb6117566110f4ec6e6c361cec4c600ebb7
Robert Peake <robert@peakepro.com>
2023-11-25 20:03:01 +0000

    rough mockup

---
+++ stclient/scripts/main.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var item:int = -1
+
+func _on_SettingsButton_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_TrackerList_item_selected(index:int) -> void:
+    item = index
+
+
+func _on_GoButton_pressed() -> void:
+	pass # Replace with function body.
